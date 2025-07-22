7a6cb7725a6b17b170b242a38ddb97a25e563671
Noof-Isa <noofisa786@gmail.com>
2025-07-22 13:40:15 +0300

    update edit and post routes for the cloudinary

--- controllers/listings.controller.js
+++ controllers/listings.controller.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const isSignedIn = require('../middleware/is-signed-in')
 const Listing = require('../models/listing')
+const upload = require('../config/multer')
+const cloudinary = require('../config/cloudinary')
 /* NEW LISTING FORM
 router.get('/new', (req, res) => {
   res.send('The /new route is working!')
@@ -13,9 +15,13 @@ router.get('/new', isSignedIn, (req, res) => {
 })
 
 // POST FORM DATA TO DATABASE
-router.post('/', isSignedIn, async (req, res) => {
+router.post('/', isSignedIn, upload.single('image'), async (req, res) => {
   try {
     req.body.seller = req.session.user._id
+    req.body.image = {
+      url: req.file.path,
+      cloudinary_id: req.file.filename
+    }
     await Listing.create(req.body)
     res.redirect('/listings')
   } catch (error) {
@@ -72,14 +78,25 @@ router.get('/:listingId/edit', isSignedIn, async (req, res) => {
 })
 
 // HANDLE EDIT FORM SUBMISSION
-router.put('/:listingId', isSignedIn, async (req, res) => {
+router.put('/:listingId', isSignedIn, upload.single('image') ,async (req, res) => {
   const foundListing = await Listing.findById(req.params.listingId).populate('seller')
+  // Check if the logged-in user is the listing owner
 
   if (foundListing.seller._id.equals(req.session.user._id)) {
-    await Listing.findByIdAndUpdate(req.params.listingId, req.body, { new: true })
-    return res.redirect(`/listings/${req.params.listingId}`)
-  }
+        // If a new image was uploaded, delete the old one from Cloudinary
+            if (req.file && foundListing.image?.cloudinary_id) {
+await cloudinary.uploader.destroy(foundListing.image.cloudinary_id)
+      foundListing.image.url = req.file.path
+      foundListing.image.cloudinary_id = req.file.filename
+    }
+   // Update listing fields
+    foundListing.title = req.body.title
+    foundListing.description = req.body.description
+    foundListing.price = req.body.price
 
+    await foundListing.save()
+        return res.redirect(`/listings/${req.params.listingId}`)
+  }
   return res.send('Not authorized')
 })
 
@@ -92,17 +109,5 @@ router.post('/:listingId/comments', isSignedIn, async (req, res) => {
   res.redirect(`/listings/${req.params.listingId}`)
 })
 
-router.put('/:listingId/comments/:commentId', isSignedIn, async (req, res) => {
-  const foundListing = await Listing.findById(req.params.listingId)
-  const comment = foundListing.comments.id(req.params.commentId)
-
-  if (comment.author.equals(req.session.user._id)) {
-    comment.content = req.body.content
-    await foundListing.save()
-    res.redirect(`/listings/${req.params.listingId}`)
-  } else {
-    res.send('Not authorized')
-  }
-})
 
 module.exports = router
